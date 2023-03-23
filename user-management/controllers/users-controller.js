ecc55a08b461ddc25ca10b64e1b72dbb7345d63a
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-23 22:23:12 +0300

    customer controllers,add customer and query customers logic

--- user-management/controllers/users-controller.js
+++ user-management/controllers/users-controller.js
@@ -38,5 +38,40 @@ module.exports = {
             console.log(error)
         }
     },
-    
+    login: async (req, res) => {
+        const { email, password } = req.body;
+        try {
+            await pool.connect()
+            let data = await pool.request()
+                .input('email', email)
+                .execute(`get_single_users`)
+            if (data.recordset.length) {
+                let dbPass = data.recordset[0].password
+                let result = await bcrypt.compare(password, dbPass)
+                console.log(result)
+                result ? res.json({ response: "Login successful" }) : res.json({ response: "Check your credentials" })
+            } else {
+                res.json({ message: 'User not found!' })
+            }
+        } catch (error) {
+            res.json(error.message)
+        }
+    },
+
+    //customers
+    addCustomer: async (req, res) => {
+        console.log("first")
+        const { fullName, phone, email } = req.body
+        try {
+            await pool.connect()
+            let data = await pool.request()
+                .input('full_name',fullName)
+                .input('email',email)
+                .input('phone',phone)
+                .execute(`add_customer`)
+            res.json(data)
+        } catch (error) {
+            res.json(error)
+        }
+    }
 }
