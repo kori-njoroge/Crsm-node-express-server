1c450c954077d06c78360e7355829a8d7fa87bf4
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-23 15:00:44 +0300

    proxy controller for the crms server, create proxys for /users,/sales,/products

--- api_logic/index.js
+++ api_logic/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
-
-
 require('dotenv').config()
+
+
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+
+// Rules for routing requests to microservices
+const salesProxy = createProxyMiddleware('/sales', { target: 'http://localhost:3001' });
+const usersProxy = createProxyMiddleware('/users', { target: 'http://localhost:3002' });
+const productsProxy = createProxyMiddleware('/products', { target: 'http://localhost:8000' });
+
+// Use the proxy middleware to forward requests to the appropriate microservice
+app.use(salesProxy);
+app.use(usersProxy);
+app.use(productsProxy);
 
 
 
 const port = process.env.PORT || 5010
-app.listen(port, console.log(`Gateway on port ${port}`))
+app.listen(port, console.log(`Gateway listening on port ${port}`))
