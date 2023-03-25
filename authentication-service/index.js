e3856a3dfb11a5e6c9e4e4e24017dc178465569d
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-25 21:13:16 +0300

    application of token w=validation middlewares and joi schema validation middlewares

--- authentication-service/index.js
+++ authentication-service/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { userRouter } = require('./routes/user-routes');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { validateJwtTokenForeign } = require('./middlewares/authenticate-middleware');
 require('dotenv').config()
 
 const app = express();
@@ -8,27 +10,34 @@ app.use(express.json());
 
 
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 // Rules for routing requests to microservices
-const productsProxy = createProxyMiddleware('/products',{ target: 'http://localhost:8000' });
-const notifyProxy = createProxyMiddleware('/notifications', { target: 'http://localhost:7000' });
+const productsProxy = createProxyMiddleware('/products',
+    {
+        target: 'http://localhost:8000',
+        onProxyReq: (proxyReq, req, res, next) => {
+            validateJwtTokenForeign(proxyReq, req, res, next)
+        }
+    })
 
-
+const notifyProxy = createProxyMiddleware('/notifications', {
+    target: 'http://localhost:7000',
+    onProxyReq: (proxyReq, req, res, next) => {
+        validateJwtTokenForeign(proxyReq, req, res, next)
+    }
+})
 
 // Use the proxy middleware to forward requests to the appropriate microservice
 app.use(productsProxy);
 app.use(notifyProxy);
 
 
-
-app.use('/users', userRouter)
-
 app.get('/users/home', (req, res) => {
     res.json({ message: 'Welcome Dawg' })
 })
 
+app.use('/users', userRouter)
 
 const port = process.env.PORT || 4010
 app.listen(port, () => { console.log(`Users service on ${port}`) })
--- authentication-service/routes/user-routes.js
+++ authentication-service/routes/user-routes.js
@@ -1,16 +1,19 @@
 const userRouter = require('express').Router()
 
-
+const { SchemaValidateMiddleware } = require('../middlewares/schema-validate')
 const { addUser, getAllUsers, login, addCustomer, getCustomers, singleCustomer, editCustomer } = require('../controllers/users-controller')
+const { addUserSchema, loginSchema, customerSchema, editCustomerDet, singleCust } = require('../services/joi-services')
+const { validateJwtTokenUsers } = require('../middlewares/authenticate-middleware')
 
 // users
-userRouter.get('/',getAllUsers)
-userRouter.post('/signup',addUser)
-userRouter.post('/login',login)
-// customer
-userRouter.post('/add-customer',addCustomer)
-userRouter.get('/all-customers',getCustomers)
-userRouter.post('/single-customer',singleCustomer)
-userRouter.post('/edit-customer',editCustomer)
+userRouter.get('/', validateJwtTokenUsers, getAllUsers)
+userRouter.post('/signup',validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, addUserSchema)}, addUser)
+userRouter.post('/login', validateJwtTokenUsers, (req, res, next) => { SchemaValidateMiddleware(req, res, next, loginSchema) }, login)
 
-module.exports={userRouter}
+// customer routes
+userRouter.get('/all-customers', validateJwtTokenUsers, getCustomers)
+userRouter.post('/add-customer',validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, customerSchema)},addCustomer)
+userRouter.post('/single-customer', validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, singleCustjoin)}, singleCustomer)
+userRouter.patch('/edit-customer', validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, editCustomerDet)}, editCustomer)
+
+module.exports = { userRouter }
