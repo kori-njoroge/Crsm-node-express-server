dd53c8c5fda3a3076d06d78257386d4449dbe2b4
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-26 22:35:57 +0300

    add category controlle rerro handling and edit on addedBy

--- products-sales service/controllers/category-controllers.js
+++ products-sales service/controllers/category-controllers.js
@@ -11,18 +11,19 @@ date = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
 module.exports = {
     // category
     createCategory: async (req, res) => {
-        const { categoryName, description } = req.body
+        const { categoryName, description, addedBy } = req.body
         try {
             await pool.connect()
             let data = await pool.request()
-                .input('category_name', categoryName.toLowerCase())
-                .input('description', description.toLowerCase())
+                .input('category_name', categoryName)
+                .input('description', description)
+                .input('added_by', addedBy)
                 .execute(`add_category`)
             data.rowsAffected.length && res.status(200).json({ message: `Successfully added new category: (${categoryName})  on ${date}` })
         } catch (error) {
             error.originalError.message.includes('Violation of UNIQUE KEY constraint') ?
                 res.status(400).json({ message: `Category {${categoryName}} already exists` }) :
-                res.status(400).json(error.originalError.message)
+                res.status(400).json(error.originalError)
         }
     },
     getAllCategories: async (req, res) => {
@@ -38,14 +39,14 @@ module.exports = {
     getSingleCategory: async (req, res) => {
         console.log(req.params)
         const { catId } = req.params
-        let id =`'${catId}'`
+        let id = `'${catId}'`
         console.log(id)
         try {
             await pool.connect()
             let data = await pool.request()
-                .input('category_id',sql.Char(6),catId)
+                .input('category_id', sql.Char(6), catId)
                 .execute(`get_category_by_id`)
-                console.log(data)
+            console.log(data)
             !data.recordset.length ? res.status(400).json({ message: "No records found" })
                 : res.status(200).json(data.recordset)
         } catch (error) {
