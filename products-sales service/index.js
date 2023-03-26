2e4bd870af103bd3ade2348ec4f4898e9cff8cf6
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-26 20:46:00 +0300

    configuration of home route, express.json parser and port set up

--- products-sales service/index.js
+++ products-sales service/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
+const express = require('express');
+const { categoryRouter } = require('./routes/category-routes');
 require('dotenv').config()
 
 
 const app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-app.get('/products', (req, res) =>{
-    console.log((" welcome first"))
-    res.json({message:"Product route reached"})
+app.get('/products', (req, res) => {
+    res.json({ message: "Product route reached" })
 })
+
+app.use('/products/category', categoryRouter)
 
 const port = process.env.PORT || 8010
 app.listen(port, console.log(`products service on port ${port}`))
