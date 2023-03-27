ff74756bbebb0eda6bcb0e53d751397a945a9fad
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-27 12:42:20 +0300

    app to use sakes Router

--- products-sales service/index.js
+++ products-sales service/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { categoryRouter } = require('./routes/category-routes');
 const { productsRouter } = require('./routes/products-routes');
+const { salesRouter } = require('./routes/sales-routes');
 require('dotenv').config()
 
 
@@ -12,8 +13,9 @@ app.get('/products', (req, res) => {
     res.json({ message: "Product route reached" })
 })
 
-app.use('/products',productsRouter)
+app.use('/products', productsRouter)
 app.use('/products/category', categoryRouter)
+app.use('/products/sales', salesRouter)
 
 const port = process.env.PORT || 8010
 app.listen(port, console.log(`products service on port ${port}`))
