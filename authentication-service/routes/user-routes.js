07b7443ed14347848b91d4e2a070b49dfa1d0cdc
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-28 22:50:56 +0300

    schema validation for axios token

--- authentication-service/routes/user-routes.js
+++ authentication-service/routes/user-routes.js
@@ -7,13 +7,13 @@ const { validateJwtTokenUsers } = require('../middlewares/authenticate-middlewar
 
 // users
 userRouter.get('/', validateJwtTokenUsers, getAllUsers)
-userRouter.post('/login',(req, res, next) => { SchemaValidateMiddleware(req, res, next, loginSchema) }, login)
-userRouter.post('/signup',(req, res, next) => {SchemaValidateMiddleware(req, res, next, addUserSchema)}, addUser)
-userRouter.patch('/update-user',validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, editCustomerDet)},updateUserDetails)
+userRouter.post('/login', (req, res, next) => { SchemaValidateMiddleware(req, res, next, loginSchema) }, login)
+userRouter.post('/signup', (req, res, next) => { SchemaValidateMiddleware(req, res, next, addUserSchema) }, addUser)
+userRouter.patch('/update-user', validateJwtTokenUsers, (req, res, next) => { SchemaValidateMiddleware(req, res, next, editCustomerDet) }, updateUserDetails)
 // customer routes
 userRouter.get('/all-customers', validateJwtTokenUsers, getCustomers)
-userRouter.post('/add-customer',validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, customerSchema)},addCustomer)
-userRouter.post('/single-customer', validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, singleCust)}, singleCustomer)
-userRouter.patch('/edit-customer', validateJwtTokenUsers,(req, res, next) => {SchemaValidateMiddleware(req, res, next, editCustomerDet)}, editCustomer)
+userRouter.post('/add-customer', validateJwtTokenUsers, (req, res, next) => { SchemaValidateMiddleware(req, res, next, customerSchema) }, addCustomer)
+userRouter.post('/single-customer', validateJwtTokenUsers, (req, res, next) => { SchemaValidateMiddleware(req, res, next, singleCust) }, singleCustomer)
+userRouter.patch('/edit-customer', validateJwtTokenUsers, (req, res, next) => { SchemaValidateMiddleware(req, res, next, editCustomerDet) }, editCustomer)
 
 module.exports = { userRouter }
