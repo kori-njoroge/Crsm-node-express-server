bf0607e9b7d8b1e3c20c3549e95c28ed62be9f2b
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-29 00:47:25 +0300

    moved employees notifications email to new route

--- background-services/controllers/customer-controllers.js
+++ background-services/controllers/customer-controllers.js
@@ -34,26 +34,5 @@ module.exports = {
         const response = await sendMail(mailOptions)
         console.log(response)
         res.status(200).json(response)
-    },
-    // User
-    addUser: async (req, res) => {
-        let data = await JSON.parse(req.headers.data)
-        const { email, fullName, role } = data[0]
-        const { crytoPassword } = data[1]
-        const mailOptions = {
-            from: process.env.USER_EMAIL,
-            to: [`${email}`],
-            subject: `Welcome to Cliethive`,
-            template: 'nowelcome',
-            context: {
-                name: `${fullName}`,
-                company: 'ClientHive',
-                role: `${role}`,
-                email: `${email}`,
-                password: `${crytoPassword}`
-            }
-        }
-        const response = await sendMail(mailOptions)
-        res.status(200).json(response)
     }
 }
