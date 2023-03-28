39ee3ac0897d3b0f90eda5773a076ee9fb7071d1
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-28 22:54:40 +0300

    allow app to use created routes for users and customers

--- background-services/index.js
+++ background-services/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const { customerRouter } = require('./routes/customer-routes');
+const path = require('path');
+const { userRouter } = require('./routes/user-routes');
 
 require('dotenv').config()
 const app = express()
 
-app.get('/notifications',(req,res) =>{
-    res.json({message:"Back grounding"})
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/notifications', (req, res) => {
+    res.json({ message: "Back grounding" })
 })
+app.use('/notifications', customerRouter)
+app.use('/notifications', userRouter)
+
 
 const port = process.env.PORT || 7010
 app.listen(port, console.log(`background services on ${port}`))
