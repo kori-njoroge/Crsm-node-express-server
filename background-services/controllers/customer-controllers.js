19fa455c4a194a8cc09aa4a3709784f5c9df0d76
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-28 22:47:53 +0300

    user mail controller

--- background-services/controllers/customer-controllers.js
+++ background-services/controllers/customer-controllers.js
@@ -5,20 +5,40 @@ require('dotenv').config()
 module.exports = {
     // customer added to the system
     customerAddedMail: async (req, res) => {
-        // const {email,fullName} = req.body
+        const { email, fullName } = await JSON.parse(req.headers?.data)
         const mailOptions = {
             from: process.env.USER_EMAIL,
-            to: 'korijunior106@gmail.com',
+            to: `${email}`,
             subject: 'Welcome',
             template: 'welcome',
-            attachments:[{filename:'company logo',path: "./assets/images/logo.png"}],
-            context:{
-                name:'Gideon',
-                company:'ClientHive'
+            attachments: [{ filename: 'company logo', path: "./assets/images/logo.png" }],
+            context: {
+                name: `${fullName}`,
+                company: 'ClientHive'
             }
         }
         let response = await sendMail(mailOptions)
-        console.log(response)
         res.json(response)
+    },
+    // User
+    addUser: async (req, res) => {
+        let data = await JSON.parse(req.headers.data)
+        const { email, fullName, role } = data[0]
+        const { crytoPassword } = data[1]
+        const mailOptions = {
+            from: process.env.USER_EMAIL,
+            to: [`${email}`],
+            subject: `Welcome to Cliethive`,
+            template: 'nowelcome',
+            context: {
+                name: `${fullName}`,
+                company: 'ClientHive',
+                role: `${role}`,
+                email: `${email}`,
+                password: `${crytoPassword}`
+            }
+        }
+        const response = await sendMail(mailOptions)
+        res.status(200).json(response)
     }
 }
