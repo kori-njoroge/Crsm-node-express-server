8963c6f7185900fbd86df8ad8bcc4fc8d12ea9ca
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-30 20:37:20 +0300

    successfully created empployees controllers for addina auser,getting all employees

--- background-services/routes/employees-routes.js
+++ background-services/routes/employees-routes.js
@@ -1,9 +1,10 @@
 
 const userRouter = require('express').Router()
 
-const { addUser, approveProduct } = require('../controllers/employee-controllers')
+const { addUser, approveProduct, approveCategory } = require('../controllers/employee-controllers')
 
 userRouter.post('/add-user', addUser)
 userRouter.post('/new-product', approveProduct)
+userRouter.post('/new-category',approveCategory)
 
 module.exports = { userRouter }
