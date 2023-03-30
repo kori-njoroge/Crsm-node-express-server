927b7fe1ecd474447dd86b43dd65930c7a09a081
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-30 20:42:10 +0300

    CORS configuration on products service

--- products-sales service/index.js
+++ products-sales service/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const { categoryRouter } = require('./routes/category-routes');
 const { productsRouter } = require('./routes/products-routes');
 const { salesRouter } = require('./routes/sales-routes');
@@ -8,6 +9,15 @@ require('dotenv').config()
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+
+app.use(cors())
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.get('/products', (req, res) => {
     res.json({ message: "Product route reached" })
